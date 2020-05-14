842c740f72d5ac6ef7e6725ca29b69c7f5716509
et1911 <et1911@etoak.com>
2020-05-14 14:02:20 +0800

    解决了冲突问题

--- Coures.java
+++ Coures.java
@@ -1,5 +1,13 @@
+<<<<<<< HEAD
 public class Course{
 	private Integer id;
 	private String CourseName;
 }
 
+=======
+public class Coures{
+	private Integer id;
+	private Integer score;
+	private String course;
+}
+>>>>>>> bab383f... 小猪的同时开发了id course score三个属性
