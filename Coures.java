9d140826789c3b96bb3003d4f525e6064d1deca9
et1911 <et1911@etoak.com>
2020-05-15 10:13:29 +0800

    pif

--- Coures.java
+++ Coures.java
@@ -1,13 +1,6 @@
-<<<<<<< HEAD
-public class Course{
-	private Integer id;
-	private String CourseName;
-}
-
-=======
 public class Coures{
 	private Integer id;
 	private Integer score;
 	private String course;
+	private String CourseName;
 }
->>>>>>> bab383f... 小猪的同时开发了id course score三个属性
