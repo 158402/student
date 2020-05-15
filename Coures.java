570f9105bd7c530138772a97ad0d095dd573fec7
et1911 <et1911@etoak.com>
2020-05-15 11:11:55 +0800

    pig同事新添加属性gender

--- Coures.java
+++ Coures.java
@@ -2,6 +2,7 @@ public class Coures{
 	private Integer id;//pig添加了注释 同事修改得注释
 	private Integer score;
 	private String course;
+	private char gender;//pig同事 添加的属性
 	private String CourseName;
 	private char gender;//pig 添加了一条新注释
 
